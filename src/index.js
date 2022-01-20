6660b841ecf98b3a1dbba3337f77a08616d41e16
jade <h_hoh@naver.com>
2022-01-20 17:48:15 +0800

    #1.0 Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { ApolloProvider } from '@apollo/client';
+import client from './apollo';
 
 ReactDOM.render(
-    <App />,
+  <ApolloProvider cleint={client}>
+    <App />
+  </ApolloProvider>
+  ,
   document.getElementById('root')
 );
