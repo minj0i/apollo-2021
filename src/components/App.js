7e4f37c0f8c8013ea7c0afecfea5ab6168cf7d18
jade <h_hoh@naver.com>
2022-01-20 15:09:37 +0800

    Revert "#1.0 Apollo Client"
    This reverts commit cba5e0dbe488a11e6b36eb1feaba8dbb44a7c67c.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,8 @@
-import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Detail from "../routes/Detail";
-import Home from "../routes/Home";
-
 function App() {
-  return (<Router>
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/:id" element={<Detail />} />
-    </Routes>
-  </Router>);
+  return (
+    <div className="App">
+    </div>
+  );
 }
 
 export default App;
