f5ca07a47ebd566f65c6093857dec1473fd6a05d
MinjeongKim <h_hoh@naver.com>
2022-03-14 23:40:58 +0800

    #0.4 Router and Styles and lint

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Detail from "../routes/Detail";
 import Home from "../routes/Home";
 
 function App() {
-  return (<Router>
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/:id" element={<Detail />} />
-    </Routes>
-  </Router>);
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { ApolloProvider } from '@apollo/client';
-import client from './apollo';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
 
 ReactDOM.render(
   <ApolloProvider cleint={client}>
     <App />
-  </ApolloProvider>
-  ,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
